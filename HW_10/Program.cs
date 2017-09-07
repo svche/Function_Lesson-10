3418f2d71ecce8d8d28bf7d16724173f866a79f5
Станислав <@->
2017-09-07 15:38:29 +0300

    HW done

---
+++ HW_10/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+//          Задача 10.7
+//Написать функцию вычисления суммы целых чисел от 1 до N.
+
+            /*
+            Console.WriteLine("Addition. Enter two numbers from one\n");
+
+            Console.Write("Enter number first: ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number second: ");
+            int second = int.Parse(Console.ReadLine());
+
+            Addition(first, second);
+            */
+
+
+//          Задача 10.8
+//Решить задачу 10.7 при помощи рекурсии
+
+            /*
+            int first = 0, second = 0;
+            Console.WriteLine("Addition");
+
+            int sum = AdditionRecurs(first, second);
+            Console.WriteLine("\tSum is: " + sum);
+            */
+
+
+            //10.9
+// Создать две функции,в которые передается массив чисел.
+//  Функции должны возвратить минимальный и максимальный элемент 
+//  массива соответственно.
+//массив передается следующим образом: void printArray(int[] array)
+
+
+
+            int[] array = new int[10];
+            for (int i = 0; i < 10; i++) array[i] = i;
+
+            PrintArrayMin(array);
+            PrintArrayMax(array);
+
+
+            Console.ReadLine();
+        }
+
+        static void PrintArrayMax(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max) max = array[i];
+            }
+                Console.WriteLine("Maximum number is: " + max);
+        }
+
+        static void PrintArrayMin(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min) min = array[i];
+            }
+                Console.WriteLine("Minimum number is: " + min);
+        }
+
+        static int AdditionRecurs(int num1, int num2)                               //task 10.8
+        {
+            Console.WriteLine("\tEnter two numbers from one\n");
+
+            Console.Write("Number first: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Number second: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 < 1 || num2 < 1) return AdditionRecurs(num1, num2);
+            else
+            {
+                int res = num1 + num2;
+                return res;
+            }
+        }
+
+        static void Addition(int num1, int num2)                               //task 10.7
+        {
+            if (num1 < 1 || num2 < 1)
+            {
+               Console.WriteLine("Mistake. Try again.");
+            }
+             else
+            {
+              Console.WriteLine("The sum is: {0}", add(num1, num2));
+            }
+        }
+        
+        static int add(int num1, int num2)
+        {
+                int result = num1 + num2;
+                return result;
+        }
+    }
+}
--- Lessons-10_0509_Methods/Program.cs
+++ Lessons-10_0509_Methods/Program.cs
@@ -10,6 +10,161 @@ namespace Lessons_10_0509_Methods
     {
         static void Main(string[] args)
         {
+            /*
+            string str = Console.ReadLine();
+            PrintSymb(str);
+            */
+
+            /*
+            int f = 6;
+            int res = sum(ref f, 2);
+            Console.WriteLine(f);
+            */
+
+            /*
+            int a = 25;
+            int b = 52;
+            Console.WriteLine("A is {0} \nB is {1}", a, b);
+
+            swap(ref a, ref b);
+
+            Console.WriteLine("\nAfter SWAP:");
+            Console.WriteLine("A is {0} \nB is {1}", a, b);
+            */
+
+            /*
+            Console.Write("Enter the symbol for repeat: ");
+            string symb = Console.ReadLine();
+
+            Console.Write("Enter count for repeating: ");
+            int count = int.Parse(Console.ReadLine());
+
+            PrintSymbCount(symb, count);
+            */
+
+            /* 
+            Console.Write("Enter the number to calculate the factorial: ");
+            int numIn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nFactorial of {0} is:  {1:###,###}.", numIn, Factorial(numIn)); 
+            */
+
+            /*
+            PrintNum(45);
+            */
+
+
+            Print("W", 50, 80, 'z');
+
+
+            Console.ReadLine();
         }
+
+        static void Print(string sym, int count, int num_border, char symb_border)
+        {
+            borderPrint(num_border, symb_border);
+            Print(sym, count);
+            borderPrint(num_border, symb_border);
+        }
+
+        static void Print(string sym, int count, int num_border)
+        {
+            borderPrint(num_border);
+            Print(sym, count);
+            borderPrint(num_border);
+        }
+
+        static void borderPrint(int num_border, char sym_border)
+        {
+            for (int i = 0; i < num_border; i++)
+            {
+                Console.Write(sym_border);
+            }
+            Console.WriteLine();
+        }
+
+        static void borderPrint(int num_border)
+        {
+            for (int i = 0; i < num_border; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(string sym, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(sym + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(string sym)                    // overload and go up^
+        {
+            Console.WriteLine(sym);
+        }
+
+        static void PrintNum(int num)                     // type definition
+        {
+            Console.WriteLine("Integer " + num);        
+        }
+
+        static void PrintNum(double num)                // type definition
+        {
+            Console.WriteLine("Double " + num);
+        }
+
+        static long Factorial(int num)       // function calls itself !!!
+        {
+            if (num == 0) return 1;
+            return num * Factorial(num - 1);
+        }
+
+        static long Factorial1(int num)
+        {
+            long resFac = 1;
+            if (num == 0) return resFac;
+
+            for (int i = 1; i <= num; i++)
+            {
+                resFac *= i;
+            }
+            return resFac;
+        }
+
+        static void PrintSymbCount(string symb, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(symb);
+            }
+        }
+
+        static void PrintSymb(string symb)
+        {
+            nextPrint();
+            Console.WriteLine(symb);
+        }
+        static void nextPrint()
+        {
+            Console.Write("The symbol is: ");
+        }
+        
+        static int sum(ref int first, int second)
+        {
+            first = 45;
+            int result = first + second;
+            return result;
+        }
+   
+        static void swap(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+
     }
 }
